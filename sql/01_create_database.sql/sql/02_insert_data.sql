036e175e62b4deda8190bf8e9280d12c4c654a08
bbbb0riss <boriskaa_shevchenko@mail.ru>
2026-08-02 22:23:34 +0300

    Update 02_insert_data.sql

--- sql/01_create_database.sql/sql/02_insert_data.sql
+++ sql/01_create_database.sql/sql/02_insert_data.sql
@@ -1,1 +1,22 @@
+USE retail_financial_analytics;
 
+INSERT INTO Categories (category_name)
+VALUES
+('Ноутбуки'),
+('Смартфоны'),
+('Планшеты'),
+('Телевизоры'),
+('Наушники'),
+('Умные часы'),
+('Игровые консоли'),
+('Бытовая техника'),
+('Комплектующие'),
+('Аксессуары');
+
+INSERT INTO Stores (store_name, city)
+VALUES
+('TechStore Москва', 'Москва'),
+('TechStore Санкт-Петербург', 'Санкт-Петербург'),
+('TechStore Казань', 'Казань'),
+('TechStore Екатеринбург', 'Екатеринбург'),
+('TechStore Новосибирск', 'Новосибирск');
